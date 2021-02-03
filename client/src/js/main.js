f72f525f3dba07bd5be110a10077d83d75eeb4f9
iamkjw77 <iamkjw77@naver.com>
2021-02-04 00:21:08 +0900

    feat: Create load img

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -1,4 +1,15 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-axios.get('http://localhost:5000/api/products')
-  .then(console.log);
+// axios.get('http://localhost:5000/api/products')
+//   .then(console.log);
+
+// import getImg from './product-detail';
+import headerRender from './header';
+import footerRender from './footer';
+import homeRender from './home';
+import getImg from './product-detail';
+
+document.addEventListener('DOMContentLoaded', headerRender);
+document.addEventListener('DOMContentLoaded', footerRender);
+document.addEventListener('DOMContentLoaded', getImg);
+// document.addEventListener('DOMContentLoaded', productRender);
